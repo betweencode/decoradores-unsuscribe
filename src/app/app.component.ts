b815f5adb12a1adf8e301d0321b4afe04f3faed1
samv <between>
2024-01-26 21:07:18 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'desuscribirangular';
+
+
+  public mifunction
+  (){
+    console.log("Hola bienv enido a mi grupo");
+  }
+}
